a9db0c7746e61753c8c8bbd5c5d169204697f0aa
whitehat-G6 <88480121+WHJR-G6@users.noreply.github.com>
2021-08-30 20:14:48 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var box;
+function setup()
+{
+ createCanvas(400,400)
+ box = createSprite(40,40,40,40);
+ box.shapeColor = color(255);
+ box.x=0;
+ box.y=150;
+ box.velocityX=5
+}
+
+function draw()
+{
+ background(220);
+ drawSprites();
+}
+
